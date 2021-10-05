c342d44c94c31ce0958c5c5fd9df29eaefe4b70e
unknown <zarina.nurbaeva@gmail.com>
2021-10-05 13:49:13 +0500

    Fist commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+int ticketPrice = 999;
+int bonusRuble = 20;
+int miles = ticketPrice / bonusRuble;
+System.out.println("Количество милей " + miles);
+    }
+}
